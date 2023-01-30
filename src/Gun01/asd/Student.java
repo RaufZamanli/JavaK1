2f26f464614f481f6bcf1e80a58177e729360b7d
RaufZamanli <rzamanli2020ps@gmail.com>
2023-01-30 20:33:42 +0400

    Task 4

--- src/Gun01/asd/Student.java
+++ src/Gun01/asd/Student.java
@@ -4,8 +4,11 @@ public class Student {
     public static void main(String[] args) {
 
         System.out.println("merhaba");
+
         System.out.println("merhaba 2");
 
         System.out.println("task 3");
+
+        System.out.println("task4 ogr");
     }
 }
